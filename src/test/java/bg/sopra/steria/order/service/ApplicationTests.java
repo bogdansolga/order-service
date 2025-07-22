8770f44adc9d4e373a7da8f34b314e375988c429
Bogdan Solga <bogdan.solga@gmail.com>
2025-07-22 10:32:48 +0300

    [dev] Use the CI profile

--- src/test/java/bg/sopra/steria/order/service/ApplicationTests.java
+++ src/test/java/bg/sopra/steria/order/service/ApplicationTests.java
@@ -2,8 +2,10 @@ package bg.sopra.steria.order.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("ci")
 class ApplicationTests {
 
 	@Test
